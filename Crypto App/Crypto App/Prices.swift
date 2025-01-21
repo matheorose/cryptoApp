921daae2234fb748d30404f39aa40bed42f48555
matheorose <matheorose1703@gmail.com>
2025-01-21 16:34:19 +0100

    update

---
+++ Crypto App/Crypto App/Prices.swift
@@ -0,0 +1,14 @@
+//
+//  Prices.swift
+//  Crypto App
+//
+//  Created by rose matheo on 21/01/2025.
+//
+
+import Foundation
+
+struct Prices {
+    var crypto: String
+    var imageName: String
+    var price: Double
+}
